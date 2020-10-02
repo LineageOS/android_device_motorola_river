fb549b9eb485f6b03808fbc3914dffd35be794c7
Andrew Hexen <SyberHexen@gmail.com>
2020-10-01 18:47:18 -0700

    river: import NFC from common
    as of change I66438c4d62384896b44190127f6ead09f640b9f4
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    
    Change-Id: Ia1671362c56fec3d8ecbe981f2453dcccdd63a99

--- device.mk
+++ device.mk
@@ -63,11 +63,25 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.river.rc
 
-# NFC
+# NFC and Secure Element packages
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp-gcf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp-gcf.conf
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.nxp.mifare.xml
+
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    NfcNci \
+    Tag \
+    SecureElement \
+    android.hardware.nfc@1.1-service \
+    android.hardware.secure_element@1.1-service-disabled
 
 # Sensors
 PRODUCT_COPY_FILES += \
