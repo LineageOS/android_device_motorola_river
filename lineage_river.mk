516fefa1117cf2efa417ee72664ab54c1f418a59
ROMSG <romsg85@gmail.com>
2024-10-20 20:58:14 -0700

    river: Adjust override prop key names
    Change-Id: I350b69da33680b2b5373354e6ef338610bef41a1

--- lineage_river.mk
+++ lineage_river.mk
@@ -6,7 +6,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-BUILD_FINGERPRINT := motorola/river_retail/river:10/QPUS30.52-33-11/df129:user/release-keys
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := river
 PRODUCT_MANUFACTURER := motorola
@@ -14,5 +13,6 @@ PRODUCT_MODEL := moto g(7)
 PRODUCT_NAME := lineage_river
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=river_retail \
-    PRIVATE_BUILDS_DESC="river_retail-user 10 QPUS30.52-33-11 df129 release-keys"
+    BuildDesc="river_retail-user 10 QPUS30.52-33-11 df129 release-keys" \
+    BuildFingerprint=motorola/river_retail/river:10/QPUS30.52-33-11/df129:user/release-keys \
+    DeviceProduct=river_retail
